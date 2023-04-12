11f77a799917ea25151531ee9ca606e24afeab34
huchanghao <2955568032@qq.com>
2023-04-12 12:31:10 +0800

    😵 ts-challenge: 4471 Zip

---
+++ packages/code/ts-challenge/03243-medium-flattendepth.ts
@@ -0,0 +1,16 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
+]
+
+
+// ============= Your Code Here =============
+type FlattenDepth = any
--- packages/code/ts-challenge/04471-medium-zip.ts
+++ packages/code/ts-challenge/04471-medium-zip.ts
@@ -1,14 +1,21 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils"
 
 type cases = [
   Expect<Equal<Zip<[], []>, []>>,
   Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
-  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[1, 2, 3], ["1", "2"]>, [[1, "1"], [2, "2"]]>>,
   Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
-  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>
 ]
 
-
 // ============= Your Code Here =============
-type Zip<T, U> = any
+type Zip<
+  T extends any[],
+  U extends any[],
+  A extends any[] =[]
+> = T extends [infer FT, ...infer RT]
+  ? U extends [infer FU, ...infer RU]
+    ? Zip<RT,RU,[...A,[FT,FU]]>
+    : A
+  : A
