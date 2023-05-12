eb2e166a91b9cb1505af8d496477f1cdfc2e0dc8
huchanghao <2955568032@qq.com>
2023-05-12 23:16:31 +0800

    😵 ts-challenge: work 05140-medium-trunc

--- packages/code/ts-challenge/05140-medium-trunc.ts
+++ packages/code/ts-challenge/05140-medium-trunc.ts
@@ -1,16 +1,17 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils"
 
 type cases = [
-  Expect<Equal<Trunc<0.1>, '0'>>,
-  Expect<Equal<Trunc<1.234>, '1'>>,
-  Expect<Equal<Trunc<12.345>, '12'>>,
-  Expect<Equal<Trunc<-5.1>, '-5'>>,
-  Expect<Equal<Trunc<'1.234'>, '1'>>,
-  Expect<Equal<Trunc<'-10.234'>, '-10'>>,
-  Expect<Equal<Trunc<10>, '10'>>,
+  Expect<Equal<Trunc<0.1>, "0">>,
+  Expect<Equal<Trunc<1.234>, "1">>,
+  Expect<Equal<Trunc<12.345>, "12">>,
+  Expect<Equal<Trunc<-5.1>, "-5">>,
+  Expect<Equal<Trunc<"1.234">, "1">>,
+  Expect<Equal<Trunc<"-10.234">, "-10">>,
+  Expect<Equal<Trunc<10>, "10">>
 ]
 
-
 // ============= Your Code Here =============
-type Trunc = any
+type Trunc<T extends number | string> = `${T}` extends `${infer F}.${number}`
+  ? F
+  : `${T}`
