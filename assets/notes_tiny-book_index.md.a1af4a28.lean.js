d76b57ceec26f56159a10b58a204e223e39571ae
Jomorx <Jomorx@users.noreply.github.com>
2023-05-13 15:38:55 +0000

    deploy: 0b47c6f0aa1c05edf27d28f20a312fac09e89177

---
+++ assets/notes_tiny-book_index.md.a1af4a28.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./app.b227e414.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"notes/tiny-book/index.md","lastUpdated":1683992283000}'),n={name:"notes/tiny-book/index.md"};function a(s,r,c,d,i,p){return o(),t("div")}const f=e(n,[["render",a]]);export{m as __pageData,f as default};
